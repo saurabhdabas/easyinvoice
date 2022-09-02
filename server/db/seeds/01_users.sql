0ed652d0ceaaa8380dff51488da20fc1ec4d9a28
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 17:37:37 -0600

    change avatar seed

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, email, password, avatar) VALUES (1,'easyinvoice@gmail.com', '1234','https://pickaface.net/gallery/avatar/Opi51c74d6edb145.png');
+INSERT INTO users (id, email, password, avatar) VALUES (1,'easyinvoice@gmail.com', '1234','https://www.filmibeat.com/img/162x203/popcorn/profile_photos/sam-worthington-20190508165423-10111.jpg');
