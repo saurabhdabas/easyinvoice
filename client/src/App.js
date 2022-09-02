85ee5c4913f2fccefda4e4118ef1e6991e3f8669
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 17:37:59 -0600

    Add protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoutes from './Hooks/useAuth';
+import './App.css';
 
 function App() {
-
+  
   return (
     <div className="App">
 
