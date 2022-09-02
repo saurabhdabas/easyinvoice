fae36d4ebd0e0df8581944460645d61f53ce38fe
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 15:51:51 -0600

    add seeds for new attribute

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- seeds/01_users.sql
 -- users seeds
-INSERT INTO users (id, email, password) VALUES (1,'easyinvoice@gmail.com', '1234');
+INSERT INTO users (id, email, password, avatar) VALUES (1,'easyinvoice@gmail.com', '1234','https://pickaface.net/gallery/avatar/Opi51c74d6edb145.png');
