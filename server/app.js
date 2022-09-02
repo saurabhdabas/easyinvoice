31cf91758e96d55d3bb35d321b9eeaf9313b36a6
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 17:39:00 -0600

    Add user authentication

--- server/app.js
+++ server/app.js
@@ -2,21 +2,38 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const db = require('./configs/db.config');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
 
 const app = express();
 
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Add user authentication
+app.post('/user-data',(req,res)=>{
+  let credentials = { 
+    email: req.body.data.email,
+    password:req.body.data.password,
+    token:'registered'
+  };
+  db.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [credentials.email,credentials.password])
+    .then(response =>{
+      response.rows[0]['token']=credentials.token
+      res.send(response.rows[0])})
+    .catch(e => console.error(e.stack));
+})
+
 
 module.exports = app;
