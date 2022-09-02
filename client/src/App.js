5556c1baf63ca10620b1c093792a23773d77ab0c
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 11:55:26 -0600

    Add Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ProtectedRoutes from './Hooks/useAuth';
 
 function App() {
+
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" 
+            element={
+              <Login/>
+            }
+        />
+        <Route path='/' 
+          element={
+            <ProtectedRoutes/>
+          }
+        >
+          <Route path="/dashboard" 
+            element={
+              <Dashboard/>
+            }
+          />
+        </Route>
+      </Routes>
+
     </div>
   );
 }
