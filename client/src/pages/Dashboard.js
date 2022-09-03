6136dc88d556a76a70160b579892de6cf9b9a5c4
Saurabh Dabas <developerdabas@gmail.com>
2022-09-02 21:06:27 -0600

    add sideNavigation

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header'
+import SideNavigation from '../components/SideNavigation'
 
 const Dashboard = () => {
   return (
     <div>
       <Header/>
+      <SideNavigation/>
     </div>
   )
 }
