030222251d36f911124f88010465da7e071a839b
Saurabh Dabas <developerdabas@gmail.com>
2022-09-03 10:44:40 -0600

    Add condition to classNames

--- client/src/components/SideNavigation.js
+++ client/src/components/SideNavigation.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
-const SideNavigation = () => {
+const SideNavigation = ({state,setState}) => {
+  console.log(state);
+  
   return (
     <div className='sideNavigation'>
       <ul className='sideNavigation__items'>
-        <li className='sideNavigation__item'>
+        <li 
+        aria-label='Dashboard' 
+        className={state.includes('Dashboard') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
+        onClick={
+        (event)=>{
+          setState([event.currentTarget.ariaLabel]);
+        }}
+        >
+          <img src='./dashboard.png' alt='list-item-logo' width='30' height='30'/>
+          <h3>Dashboard</h3>
+        </li>
+        <li 
+        aria-label='Customers' 
+        className={state.includes('Customers') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
+        onClick={
+        (event)=>setState([event.currentTarget.ariaLabel])}
+        >
           <img src='./customer.png' alt='list-item-logo' width='30' height='30'/>
           <h3>Customers</h3>
         </li>
-        <li className='sideNavigation__item'>
+        <li 
+        aria-label='Invoices' 
+        className={state.includes('Invoices') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
+        onClick={
+        (event)=>setState([event.currentTarget.ariaLabel])}
+        >
           <img src='./invoice.png' alt='list-item-logo' width='30' height='30'/>
           <h3>Invoices</h3>
         </li>
