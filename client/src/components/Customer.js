faec83ff4f493ccad9ec36d80073e10324b76929
Saurabh Dabas <developerdabas@gmail.com>
2022-09-04 18:38:09 -0600

    Add props to the customer component

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Customer = () => {
+const Customer = ({name,country,city,province,company,taxnumber}) => {
   return (
     <div className='customer'>
       <div className='customer__header'>
         <div className='customer__logo'>
-          <h2>P</h2>
+          <h2>{company[0]}</h2>
         </div>
         <div className='customer__name'>
-          <h3>Abigail</h3>
-          <h3>Portronics Ltd.</h3>
+          <h3>{name}</h3>
+          <h3>{company}</h3>
         </div>
       </div>
       <div className='customer__main'>
         <div className='customer__taxnumber'>
           <p>Tax #</p>
-          <p>037-9865</p>
+          <p>{taxnumber}</p>
         </div>
         <div className='customer__address'>
           <p>Location</p>
-          <p>Edmonton,Alberta,Canada</p>
+          <p>{`${city},${province},${country}`}</p>
         </div>
       </div>
       <div className='customer__footer'>
