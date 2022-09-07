1e53d164495a422375d877fc0f9aea873a1798f5
Saurabh Dabas <developerdabas@gmail.com>
2022-09-06 21:56:42 -0600

    insert the formdata into the database

--- server/app.js
+++ server/app.js
@@ -18,7 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/', indexRouter);
 app.use('/customers', customersRouter(db));
 
@@ -34,6 +33,27 @@ app.post('/user-data',(req,res)=>{
       response.rows[0]['token']=credentials.token
       res.send(response.rows[0])})
     .catch(e => console.error(e.stack));
+})
+// Add the new client data to database
+app.post('/client-data',(req,res)=>{
+  console.log(req.body.data)
+  let formData = {
+    name:req.body.data.name,
+    email:req.body.data.email,
+    country:req.body.data.country,
+    street:req.body.data.street,
+    city:req.body.data.city,
+    province:req.body.data.province,
+    zipcode:req.body.data.zipcode,
+    company:req.body.data.company,
+    taxnumber:req.body.data.taxnumber
+  }
+  db.query(
+    `INSERT INTO customers (name,email,country,street,city,province,zipcode,company,taxnumber)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;`,
+    [formData.name,formData.email,formData.country,formData.street,formData.city,formData.province,formData.zipcode,formData.company,formData.taxnumber])
+  .then((response) => res.send(response))
+  .catch((error) => res.send(error));
 })
 
 
