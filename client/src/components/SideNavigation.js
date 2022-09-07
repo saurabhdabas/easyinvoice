563231bc70df7077e38cded00769ce5271219b32
Saurabh Dabas <developerdabas@gmail.com>
2022-09-07 17:18:01 -0600

    Add path for customers component

--- client/src/components/SideNavigation.js
+++ client/src/components/SideNavigation.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import {useNavigate} from 'react-router-dom'
 const SideNavigation = ({state,setState}) => {
   console.log(state);
-  
+  const navigate = useNavigate()
   return (
     <div className='sideNavigation'>
       <ul className='sideNavigation__items'>
@@ -21,7 +21,10 @@ const SideNavigation = ({state,setState}) => {
         aria-label='Customers' 
         className={state.includes('Customers') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
         onClick={
-        (event)=>setState([event.currentTarget.ariaLabel])}
+          (event)=>{
+            setState([event.currentTarget.ariaLabel])
+            navigate('/customers');
+          }}
         >
           <img src='./customer.png' alt='list-item-logo' width='30' height='30'/>
           <h3>Customers</h3>
@@ -30,7 +33,9 @@ const SideNavigation = ({state,setState}) => {
         aria-label='Invoices' 
         className={state.includes('Invoices') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
         onClick={
-        (event)=>setState([event.currentTarget.ariaLabel])}
+        (event)=>{
+          setState([event.currentTarget.ariaLabel])
+        }}
         >
           <img src='./invoice.png' alt='list-item-logo' width='30' height='30'/>
           <h3>Invoices</h3>
