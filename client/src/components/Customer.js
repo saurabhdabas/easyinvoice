b3de76746379106100e47b59a748f2971edb0fb3
Saurabh Dabas <developerdabas@gmail.com>
2022-09-09 17:03:35 -0600

    restructured customer component

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -4,22 +4,25 @@ const Customer = ({name,country,city,province,company,taxnumber}) => {
   return (
     <div className='customer'>
       <div className='customer__header'>
-        <div className='customer__logo'>
-          <h2>{company[0]}</h2>
+        <div className='customer__logos'>
+          <img src='/id-card.png' alt='name-logo' width='20' height='20'/>
+          <p>Client</p>
+          <img src='/office-building.png' alt='company-logo' width='20' height='20'/>
+          <p>Company</p>
         </div>
         <div className='customer__name'>
-          <h3>{name}</h3>
-          <h3>{company}</h3>
+          <h4>{name.toLowerCase()}</h4>
+          <h4>{company.toLowerCase()}</h4>
         </div>
       </div>
       <div className='customer__main'>
         <div className='customer__taxnumber'>
-          <p>Tax #</p>
+          <h4>Tax #</h4>
           <p>{taxnumber}</p>
         </div>
         <div className='customer__address'>
-          <p>Location</p>
-          <p>{`${city},${province},${country}`}</p>
+          <h4>Location</h4>
+          <p>{`${city.toLowerCase()},${province.toLowerCase()},${country.toLowerCase()}`}</p>
         </div>
       </div>
       <div className='customer__footer'>
