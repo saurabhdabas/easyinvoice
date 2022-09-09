c4c1af1536eea136bf96b6c94d9cdb1091d11936
Saurabh Dabas <developerdabas@gmail.com>
2022-09-08 21:57:21 -0600

    add catch statement

--- server/db/seeds/02_customers.sql
+++
@@ -1,3 +0,0 @@
--- seeds/02_customers.sql
--- customers seeds
-INSERT INTO customers (id, name, email, street, city, province, zipcode, country, company, taxnumber) VALUES (1, 'abigail','abigailmalfoy@gmail.com', '28 Ave NW', 'Edmonton','Alberta','T3N 98D','Canada','Portronix Ltd.','037-8549');
