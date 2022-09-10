21560773a55d66e01f5ead691c95163584ddf734
Saurabh Dabas <developerdabas@gmail.com>
2022-09-10 09:45:30 -0600

    Remove search bar from header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,6 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 const Header = () => {
-
-  const [search,setSearch] = useState('');
-  const [category,setCategory] = useState('');
 
   const clearLocalStorage = () => {
     localStorage.removeItem('user');
@@ -15,23 +12,6 @@ const Header = () => {
         <span className='header__brand'>
           <img src='./bill.png' alt='brand-logo' width='50' height='50'/>
           <h2>Easy Invoice</h2>
-        </span>
-        <span className='header__search'>
-          <div className='header__search-wrapper'>
-            <img src='./search.png' alt='search-icon' width='25' height='25'/>
-            <input className='header__datalist' name="category" list="categories" placeholder="Select a Category" value={category} onChange={(event)=>setCategory(event.target.value)}/>
-            <datalist id="categories">
-              <option value="Customers"/>
-              <option value="Invoices"/>
-            </datalist>
-            <input 
-              className='header__search-input' 
-              type='text' 
-              placeholder='Enter your search'
-              value={search} 
-              onChange={(event)=>setSearch(event.target.value)}
-            />
-          </div>
         </span>
         <Link className="header__button" to="/" onClick={clearLocalStorage}>
           <img src={JSON.parse(localStorage.getItem('user')).avatar} alt='avatar' width='50' height='40'/>
