bd0426514b209d9a498f85379b7e2ae63971d3cb
Saurabh Dabas <developerdabas@gmail.com>
2022-09-10 09:47:57 -0600

    Handle customers update

--- client/src/components/Customers.js
+++ client/src/components/Customers.js
@@ -2,37 +2,60 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import Customer from './Customer';
 import AddCustomer from './AddCustomer';
-
+import SearchBar from './SearchBar';
 const Customers = () => {
   
-  const [customers,setCustomers] = useState([]);
+  const [customers,setCustomers] = useState({list:[]});
+  const [showForm,setShowForm] = useState(false);
 
   useEffect(()=>{
     axios.get('http://localhost:8080/customers')
     .then((response)=>{
-      setCustomers((prev)=>[...prev,response.data.customers])
+      
+      if(response.data.length){
+        setCustomers(({...customers,list:response.data}))
+      }
     })
     .catch((err)=>`The Error is:${err}`);
-  },[])
-
+  },[customers.list.length])
+  console.log("customers:",customers)
   return(
   <div className='customers'>
-    {customers.length ? 
+    {customers.list.length ?
     <React.Fragment>
-      {customers[0].map((customer)=>
-        <Customer 
-          key={customer.id} 
-          name={customer.name} 
-          country={customer.country}
-          city={customer.city}
-          province={customer.province}
-          company={customer.company}
-          taxnumber={customer.taxnumber}
-        />
-      )}
-      <AddCustomer/>
-    </React.Fragment> : 
-    <AddCustomer/>}
+      <div className='customers__list-wrapper'>
+        <div className='customers__list'>
+          <SearchBar/>
+          {customers.list.map((customer)=>
+            <Customer 
+              key={customer.id} 
+              name={customer.name} 
+              country={customer.country}
+              city={customer.city}
+              province={customer.province}
+              company={customer.company}
+              taxnumber={customer.taxnumber}
+            />
+          )}
+        </div>
+      </div>
+      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm}/>
+    </React.Fragment>
+    : 
+    <React.Fragment>
+      <div className='customers__list-wrapper'>
+        <SearchBar/>
+        <div className='customers__list'>
+          <p>Click&nbsp;
+            <span><img src='./plus.png' alt='add-icon' width='20' height='20'/></span> 
+            &nbsp;Icon to get started 
+          </p>
+        </div>
+      </div>
+      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm}/>
+    </React.Fragment>
+    }
+
   </div>)
 }
 
