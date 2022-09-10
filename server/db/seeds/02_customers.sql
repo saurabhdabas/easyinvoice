2d9c207a06ca984086556c225897ebc40ffc8c1f
Saurabh Dabas <developerdabas@gmail.com>
2022-09-10 09:41:09 -0600

    Add seeds for customers

---
+++ server/db/seeds/02_customers.sql
@@ -0,0 +1,3 @@
+-- seeds/02_customers.sql
+-- users customers
+-- INSERT INTO customers (id, name, email, country, street, city, province, zipcode, company, taxnumber) VALUES (1,'Tenshin', 'tenshin@gmail.com','Canada', '56 ST NW', 'Calgary', 'Alberta','T3N 0P3', 'Intuit', '063-8765');
