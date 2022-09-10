46807d6177b6f968ca05b576dcec4471d8e5e770
Saurabh Dabas <developerdabas@gmail.com>
2022-09-10 14:59:39 -0600

    Fix delete client functionality

--- client/src/components/Customers.js
+++ client/src/components/Customers.js
@@ -7,17 +7,38 @@ const Customers = () => {
   
   const [customers,setCustomers] = useState({list:[]});
   const [showForm,setShowForm] = useState(false);
+  const [deleteCustomerId,setDeleteCustomerId] = useState(0);
+  const [updateCustomer,setUpdateCustomer] = useState(false);
+  const [inputs,setInputs]=useState({
+    name:'',
+    email:'',
+    country:'',
+    street:'',
+    city:'',
+    province:'',
+    zipcode:'',
+    company:'',
+    taxnumber:''
+  })
 
   useEffect(()=>{
     axios.get('http://localhost:8080/customers')
-    .then((response)=>{
-      
+    .then((response)=> {
       if(response.data.length){
         setCustomers(({...customers,list:response.data}))
       }
     })
     .catch((err)=>`The Error is:${err}`);
   },[customers.list.length])
+
+  useEffect(()=>{
+    if(deleteCustomerId){
+      axios.put(`http://localhost:8080/customers/${deleteCustomerId}/delete`,{customerId:deleteCustomerId})
+      .then((response)=>{
+        setCustomers({...customers,list:response.data.rows})
+      })
+    }
+  },[deleteCustomerId])
 
   return(
   <div className='customers'>
@@ -28,20 +49,28 @@ const Customers = () => {
           <SearchBar/>
           {customers.list.map((customer)=>
             <Customer 
-              key={customer.id} 
+              key={customer.id}
+              id={customer.id} 
               name={customer.name} 
               country={customer.country}
               city={customer.city}
               province={customer.province}
               company={customer.company}
               taxnumber={customer.taxnumber}
+              setShowForm={setShowForm}
+              deleteCustomerId={deleteCustomerId}
+              setDeleteCustomerId={setDeleteCustomerId}
+              updateCustomer={updateCustomer}
+              setUpdateCustomer={setUpdateCustomer}
               customers={customers}
               setCustomers={setCustomers}
+              inputs={inputs}
+              setInputs={setInputs}
             />
           )}
         </div>
       </div>
-      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm}/>
+      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm} inputs={inputs} setInputs={setInputs} updateCustomer={updateCustomer} setUpdateCustomer={setUpdateCustomer}/>
     </React.Fragment>
     : 
     <React.Fragment>
@@ -54,7 +83,7 @@ const Customers = () => {
           </p>
         </div>
       </div>
-      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm}/>
+      <AddCustomer customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm} inputs={inputs} setInputs={setInputs} updateCustomer={updateCustomer} setUpdateCustomer={setUpdateCustomer}/>
     </React.Fragment>
     }
 
