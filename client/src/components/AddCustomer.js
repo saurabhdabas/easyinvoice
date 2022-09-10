8c310e7feabd699ab0f5c3da9bdb3a687535e1e5
Saurabh Dabas <developerdabas@gmail.com>
2022-09-10 09:43:29 -0600

    Refactor code and inport AddCustomer Form conponent

--- client/src/components/AddCustomer.js
+++ client/src/components/AddCustomer.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom'
+import AddCustomerForm from './AddCustomerForm';
 
 
-const AddCustomer = ({setIndex}) => {
-  const navigate = useNavigate()
-  const handleClick = () => {
-    setIndex((index)=>index + 1);
-    navigate('/newclient')
-  }
-  
+const AddCustomer = ({customers,setCustomers,showForm,setShowForm}) => {  
   return (
-    <div className='customers__AddCustomer'>
-      <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={handleClick}/>
-      <h2>Add a customer</h2>
-    </div>
+    <React.Fragment>
+      {!showForm ?
+        <div className='customers__AddCustomer'>
+          <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={()=>setShowForm(true)}/>
+          <h2>Add a new customer</h2>
+        </div> : 
+        <AddCustomerForm customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm}/>
+      }
+    </React.Fragment>
   )
 }
 
