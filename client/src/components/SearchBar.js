cd44b5e64bbbe591e8810baed28dd327200b95d7
Saurabh Dabas <developerdabas@gmail.com>
2022-09-14 15:22:08 -0600

    Add search functionality

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,7 +1,12 @@
-import React,{useState} from 'react';
-
-const SearchBar = () => {
-  const [search,setSearch] = useState('');
+import React, { useEffect } from 'react';
+import searchcustomers from '../Helpers/searchcustomers';
+const SearchBar = ({search,setSearch,customers,setSearchedCustomer}) => {
+  useEffect(()=>{
+    searchcustomers(search,customers)
+    if(search){
+      setSearchedCustomer([...searchcustomers(search,customers)]);
+    }
+  },[search])
   return (
     <div className='search'>
       <img src='./loupe.png' alt='search-icon' width='25' height='25'/>
@@ -10,7 +15,9 @@ const SearchBar = () => {
         type='text' 
         placeholder='Search Customers'
         value={search} 
-        onChange={(event)=>setSearch(event.target.value)}
+        onChange={(event)=>{
+          setSearch(event.target.value)
+        }}
       />
     </div>
   )
