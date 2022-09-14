864025d482fa53f1907594f3e8ee34740360dfb0
Saurabh Dabas <developerdabas@gmail.com>
2022-09-14 10:26:33 -0600

    Reset form

--- client/src/components/AddCustomer.js
+++ client/src/components/AddCustomer.js
@@ -7,7 +7,20 @@ const AddCustomer = ({customers,setCustomers,showForm,setShowForm,inputs,setInpu
     <React.Fragment>
       {!showForm ?
         <div className='customers__AddCustomer'>
-          <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={()=>setShowForm(true)}/>
+          <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={()=>{
+            setInputs({
+              name:'',
+              email:'',
+              country:'',
+              street:'',
+              city:'',
+              province:'',
+              zipcode:'',
+              company:'',
+              taxnumber:''
+            })
+            setShowForm(true)
+            }}/>
           <h2>Add a new customer</h2>
         </div> : 
         <AddCustomerForm customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm} inputs={inputs} setInputs={setInputs} updateCustomerId={updateCustomerId} setUpdateCustomerId={setUpdateCustomerId}/>
