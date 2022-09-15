b48f6e3149ff40c4ff56cfffb05c902e7d0bf069
Saurabh Dabas <developerdabas@gmail.com>
2022-09-14 19:45:12 -0600

    Add voice control to search bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,12 +1,34 @@
 import React, { useEffect } from 'react';
 import searchcustomers from '../Helpers/searchcustomers';
-const SearchBar = ({search,setSearch,customers,setSearchedCustomer}) => {
+const SearchBar = ({search,setSearch,customers,setSearchedCustomer,isListening,setIsListening}) => {
+  const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+  const speech = new SpeechRecognition();
+  speech.continuous = false;
+
   useEffect(()=>{
     searchcustomers(search,customers)
     if(search){
       setSearchedCustomer([...searchcustomers(search,customers)]);
     }
   },[search])
+
+  const handleVoiceCommand = () => {
+    !isListening ? setIsListening(true) : setIsListening(false)
+  }
+  useEffect(() => {
+    if(isListening){
+      speech.start();
+      speech.onresult = event => {
+        setSearch(event.results[event.results.length - 1][0].transcript)
+      };
+    } else{
+      speech.stop();
+    }
+    speech.onend = function(event) {
+      //Fired when the speech recognition service has disconnected.
+      setIsListening(false);
+    }
+   },[isListening]);
   return (
     <div className='search'>
       <img src='./loupe.png' alt='search-icon' width='25' height='25'/>
@@ -19,6 +41,12 @@ const SearchBar = ({search,setSearch,customers,setSearchedCustomer}) => {
           setSearch(event.target.value)
         }}
       />
+      <div className='search__voice-icon'>
+        <img src={isListening ?'./google-voice.png': './microphone-off.png'}alt='search-icon' width='25' height='25' onClick={handleVoiceCommand}/>
+        <svg height="40" width="40" className={isListening ? 'blinking' :'hidden'}>
+          {isListening ? <circle cx="50%" cy="50%" r="5" fill="crimson" />: null}
+        </svg>
+      </div> 
     </div>
   )
 }
