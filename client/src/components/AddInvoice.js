24726b9a825dac3533a3f7518dd4da78fb631df3
Saurabh Dabas <developerdabas@gmail.com>
2022-09-16 00:08:26 -0600

    create addInvoice compo

---
+++ client/src/components/AddInvoice.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import AddInvoiceForm from './AddInvoiceForm';
+
+
+const AddInvoice = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm,invoiceInputs,setInvoiceInputs,updateInvoiceId,setUpdateInvoiceId}) => {  
+  return (
+    <React.Fragment>
+      {!showInvoiceForm ?
+        <div className='invoices__AddInvoice'>
+          <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={()=>{
+            setInvoiceInputs({
+              name:'',
+              email:'',
+              country:'',
+              street:'',
+              city:'',
+              province:'',
+              zipcode:'',
+              company:'',
+              taxnumber:'',
+              title:'',
+              date:'',
+              subtotal:0,
+              balance:0,
+              message:'Thank You for your business!',
+              tabledata:{
+                rOne:{
+                  description:'',quantity:0,unitprice:0,total:0},
+                rTwo:{
+                  description:'',quantity:0,unitprice:0,total:0},
+                rThree:{
+                  description:'',quantity:0,unitprice:0,total:0},
+              }
+            })
+            setShowInvoiceForm(true)
+            }}/>
+          <h2>Generate an Invoice</h2>
+        </div> : 
+        <AddInvoiceForm invoices={invoices} setInvoices={setInvoices} showInvoiceForm={showInvoiceForm} setShowInvoiceForm={setShowInvoiceForm} invoiceInputs={invoiceInputs} setInvoiceInputs={setInvoiceInputs} updateInvoiceId={updateInvoiceId} setUpdateInvoiceId={setUpdateInvoiceId}/>
+      }
+    </React.Fragment>
+  )
+}
+
+export default AddInvoice
