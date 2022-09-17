c1303fde7a47a1ad57ff5684106ef1f36008f789
Saurabh Dabas <developerdabas@gmail.com>
2022-09-17 14:27:00 -0600

    Add state argument to display

--- client/src/Helpers/display.js
+++ client/src/Helpers/display.js
@@ -1,15 +1,23 @@
 import Customers from "../components/Customers";
 import Invoices from "../components/Invoices";
 import Dashboard from "../components/Dashboard";
+import DetailedInvoice from "../components/DetailedInvoice";
+import DetailedCustomer from "../components/DetailedCustomer";
 
-const display = (component) => {
+const display = (component,setState) => {
   switch(component) {
     case
      "Customers":
-      return <Customers/>
+      return <Customers state={component} setState={setState}/>
     case
      "Invoices":
-      return <Invoices/> 
+      return <Invoices state={component} setState={setState}/> 
+    case
+      "DetailedInvoice":
+       return <DetailedInvoice state={component}/>
+    case
+      "DetailedCustomer":
+       return <DetailedCustomer state={component}/>  
     default: 
     return <Dashboard/>  
   }
