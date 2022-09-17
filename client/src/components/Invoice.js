cec8d2d867ed606e88adc2eefbcd2ae7c01cf4af
Saurabh Dabas <developerdabas@gmail.com>
2022-09-17 14:26:06 -0600

    update props for Invoice

--- client/src/components/Invoice.js
+++ client/src/components/Invoice.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Invoice = ({id,name,company,date,balance,setShowInvoiceForm,setDeleteInvoiceId,setUpdateInvoiceId}) => {
-
-
+const Invoice = ({id,name,setState,date,balance,setShowInvoiceForm,setDeleteInvoiceId,setUpdateInvoiceId}) => {
+  const navigate = useNavigate();
+  const handleStateChange = () => {
+    navigate(`/invoices/${id}`)
+    setState(['DetailedInvoice'])
+  }
   return (
     <div className='invoice'>
       <div className='invoice__header'>
@@ -16,23 +20,26 @@ const Invoice = ({id,name,company,date,balance,setShowInvoiceForm,setDeleteInvoi
       </div>
       <div className='invoice__main'>
         <div className='invoice__number'>
-          <p><a href={`/invoices/${id}`}>INV-000{id}</a>&nbsp;|&nbsp;<span>{date}</span></p>
+          <p>INV-000{id}&nbsp;|&nbsp;<span>{date}</span></p>
         </div>
         <div className='invoice__balance'>
           <p>$&nbsp;{balance}</p>
         </div>
       </div>
       <div className='invoice__footer'>
-        <button className='invoice__edit' type='submit' id={id} onClick={
-          (event)=>{
-            setUpdateInvoiceId(event.currentTarget.id)
-            setShowInvoiceForm(true)
-          }}>Edit</button>
-        <button className='invoice__delete' type='submit' id={id} onClick={
-          (event)=>{
-            setDeleteInvoiceId(event.currentTarget.id)
-          }
-          }>Delete</button>
+        <div className='invoice__footer-btns'>
+          <button className='invoice__edit' type='submit' id={id} onClick={
+            (event)=>{
+              setUpdateInvoiceId(event.currentTarget.id)
+              setShowInvoiceForm(true)
+            }}>Edit</button>
+          <button className='invoice__delete' type='submit' id={id} onClick={
+            (event)=>{
+              setDeleteInvoiceId(event.currentTarget.id)
+            }
+            }>Delete</button>
+        </div>
+        <button className='invoice__details' onClick={handleStateChange} id={id}>Details</button>
       </div>
     </div>
   )
