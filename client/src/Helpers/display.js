6de6a3961bbd30c5a4f64b34b6e4390d9f5b3b14
Saurabh Dabas <developerdabas@gmail.com>
2022-09-18 00:28:27 -0600

    Add props to detailed Invoice

--- client/src/Helpers/display.js
+++ client/src/Helpers/display.js
@@ -4,17 +4,17 @@ import Dashboard from "../components/Dashboard";
 import DetailedInvoice from "../components/DetailedInvoice";
 import DetailedCustomer from "../components/DetailedCustomer";
 
-const display = (component,setState) => {
+const display = (component,setState,invoiceId,setInvoiceId) => {
   switch(component) {
     case
      "Customers":
       return <Customers state={component} setState={setState}/>
     case
      "Invoices":
-      return <Invoices state={component} setState={setState}/> 
+      return <Invoices state={component} setState={setState} setInvoiceId={setInvoiceId}/> 
     case
       "DetailedInvoice":
-       return <DetailedInvoice state={component}/>
+       return <DetailedInvoice state={component} invoiceId={invoiceId}/>
     case
       "DetailedCustomer":
        return <DetailedCustomer state={component}/>  
