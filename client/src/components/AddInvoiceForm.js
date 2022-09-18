48921e05add77248609376a6cc94dfbce6e1cffe
Saurabh Dabas <developerdabas@gmail.com>
2022-09-18 00:29:12 -0600

    props handling for detailed invoice

--- client/src/components/AddInvoiceForm.js
+++ client/src/components/AddInvoiceForm.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import axios from 'axios';
 import findSubtotal from '../Helpers/findSubtotal';
 const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm,invoiceInputs,setInvoiceInputs,updateInvoiceId,setUpdateInvoiceId}) => {
-
+  
   useEffect(()=>{
     setInvoiceInputs({...invoiceInputs,subtotal:findSubtotal(invoiceInputs,'tabledata')})
   },[invoiceInputs.tabledata])
@@ -23,16 +23,13 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
         setShowInvoiceForm(false)
       })
     }
-    else{
+    else {
       axios.post('http://localhost:8080/invoices/add',{data:invoiceInputs})
       .then((response)=>{
-        console.log("response:",response.data.rows)
-        axios.get('http://localhost:8080/invoices')
-        .then((response)=> {
-          setInvoices(({...invoices,list:response.data}))
-          setShowInvoiceForm(false)
-          console.log('invoices:',invoices);
-        })
+        console.log("Invoices:",invoices);
+        console.log("Response:",response);
+        setInvoices({...invoices,list:response.data.rows})
+        setShowInvoiceForm(false)
       })
     }
   } 
@@ -49,8 +46,11 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
       zipcode:'',
       company:'',
       taxnumber:'',
+      phone:'',
       title:'',
       date:'',
+      duedate:'',
+      notes:'',
       subtotal:0,
       balance:0,
       message:'Thank You for your business!',
@@ -88,70 +88,84 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
             <label htmlFor='company'><h3>Company</h3></label>
             <img src='/office.png' alt='company-logo' width='20' height='20'/>
           </span>
-          <input id='name' type='text' name='company' value={invoiceInputs.company} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,company:event.target.value})} placeholder='Enter a company name'/>
+          <input id='name' type='text' name='company' value={invoiceInputs.company} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,company:event.target.value})} placeholder='Microsoft'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='taxnumber'><h3>Tax number</h3></label>
             <img src='/tax.png' alt='taxnumber-logo' width='20' height='20'/>
           </span>
-          <input id='taxnumber' type='text' name='taxnumber' value={invoiceInputs.taxnumber} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,taxnumber:event.target.value})} placeholder='Enter your tax number'/>
+          <input id='taxnumber' type='text' name='taxnumber' value={invoiceInputs.taxnumber} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,taxnumber:event.target.value})} placeholder='002-9865'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='email'><h3>Email id</h3></label>
             <img src='/invoice-id.png' alt='taxnumber-logo' width='20' height='20'/>
           </span>
-          <input id='email' type='text' name='email' value={invoiceInputs.email} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,email:event.target.value})} placeholder='Enter the email id'/>
+          <input id='email' type='text' name='email' value={invoiceInputs.email} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,email:event.target.value})} placeholder='janethomas@gmail.com'/>
+        </div>
+        <div className='invoice-info__input'>
+          <span>
+            <label htmlFor='phone'><h3>Phone number</h3></label>
+            <img src='/tag.png' alt='invoice-logo' width='20' height='20'/>
+          </span>
+          <input id='phone' type='tel' name='phone' value={invoiceInputs.phone} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,phone:event.target.value})} placeholder='438-650-0271'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='date'><h3>Invoice Date</h3></label>
             <img src='/calendar.png' alt='date-logo' width='20' height='20'/>
           </span>
-          <input id='date' type='date' name='date' value={invoiceInputs.date} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,date:event.target.value})} placeholder='janethomas@gmail.com'/>
+          <input id='date' type='date' name='date' value={invoiceInputs.date} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,date:event.target.value})}/>
+        </div>
+        <div className='invoice-info__input'>
+          <span>
+            <label htmlFor='duedate'><h3>Due Date</h3></label>
+            <img src='/calendar.png' alt='duedate-logo' width='20' height='20'/>
+          </span>
+          <input id='duedate' type='date' name='duedate' value={invoiceInputs.duedate} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,duedate:event.target.value})} />
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='title'><h3>Invoice title</h3></label>
-            <img src='/tag.png' alt='invoice-logo' width='20' height='20'/>
+            <img src='/tag.png' alt='title-logo' width='20' height='20'/>
           </span>
-          <input id='title' type='text' name='title' value={invoiceInputs.title} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,title:event.target.value})} placeholder='Enter an invoice title'/>
+          <input id='title' type='text' name='title' value={invoiceInputs.title} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,title:event.target.value})} placeholder='website redesigning'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='street'><h3>Street</h3></label>
             <img src='/street.png' alt='street-logo' width='20' height='20'/>
           </span>
-          <input id='street' type='text' name='street' value={invoiceInputs.street} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,street:event.target.value})} placeholder='Enter your street address'/>
+          <input id='street' type='text' name='street' value={invoiceInputs.street} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,street:event.target.value})} placeholder='56 Ave NW'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='city'><h3>City</h3></label>
             <img src='/city.png' alt='city-logo' width='20' height='20'/>
           </span>
-          <input id='city' type='text' name='city' value={invoiceInputs.city} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,city:event.target.value})} placeholder='Enter your city'/>
+          <input id='city' type='text' name='city' value={invoiceInputs.city} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,city:event.target.value})} placeholder='Calgary'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='zipcode'><h3>Zipcode</h3></label>
             <img src='/pincode.png' alt='zipcode-logo' width='20' height='20'/>
           </span>
-          <input id='zipcode' type='text' name='zipcode' value={invoiceInputs.zipcode} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,zipcode:event.target.value})} placeholder='Enter your zipcode'/>
+          <input id='zipcode' type='text' name='zipcode' value={invoiceInputs.zipcode} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,zipcode:event.target.value})} placeholder='T3K 0T4'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='province'><h3>Province</h3></label>
             <img src='/state.png' alt='province-logo' width='20' height='20'/>
           </span>
-          <input id='province' type='text' name='province' value={invoiceInputs.province} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,province:event.target.value})} placeholder='Enter your province'/>
+          <input id='province' type='text' name='province' value={invoiceInputs.province} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,province:event.target.value})} placeholder='Alberta'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='country'><h3>Country</h3></label>
             <img src='/flag.png' alt='country-logo' width='20' height='20'/>
           </span>
-          <input id='country' type='text' name='country' value={invoiceInputs.country} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,country:event.target.value})} placeholder='Enter your country'/>
+          <input id='country' type='text' name='country' value={invoiceInputs.country} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,country:event.target.value})} placeholder='Canada'/>
         </div>        
         <div className='invoice-info__input'>
           <span>
@@ -159,6 +173,13 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
             <img src='/message.png' alt='country-logo' width='20' height='20'/>
           </span>
           <input id='message' type='text' name='message' value={invoiceInputs.message} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,message:event.target.value})} placeholder='Enter your message'/>
+        </div>
+        <div className='invoice-info__input'>
+          <span>
+            <label htmlFor='remarks'><h3>Remarks</h3></label>
+            <img src='/message.png' alt='country-logo' width='20' height='20'/>
+          </span>
+          <input id='remarks' type='text' name='remarks' value={invoiceInputs.notes} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,notes:event.target.value})} placeholder='Additional Remarks'/>
         </div>
         <div className='invoice-info__input-table'>
           <div>
