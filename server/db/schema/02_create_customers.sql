3805846d055ad50106f539dbde6f2b2ec99557f4
Saurabh Dabas <developerdabas@gmail.com>
2022-09-19 09:11:27 -0600

    Add new attributes to customers

--- server/db/schema/02_create_customers.sql
+++ server/db/schema/02_create_customers.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  photo VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
@@ -11,5 +12,6 @@ CREATE TABLE customers (
   province VARCHAR(255) NOT NULL,
   zipcode VARCHAR(255) NOT NULL,
   company VARCHAR(255) NOT NULL,
+  logo VARCHAR(255) NOT NULL,
   taxnumber VARCHAR(255) NOT NULL
 );
