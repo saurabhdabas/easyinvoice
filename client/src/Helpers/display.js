ab0c6b112eb3cf6aec55d0b64862a8c7bf34bcdc
Saurabh Dabas <developerdabas@gmail.com>
2022-09-19 09:06:07 -0600

    Add props to components

--- client/src/Helpers/display.js
+++ client/src/Helpers/display.js
@@ -4,20 +4,20 @@ import Dashboard from "../components/Dashboard";
 import DetailedInvoice from "../components/DetailedInvoice";
 import DetailedCustomer from "../components/DetailedCustomer";
 
-const display = (component,setState,invoiceId,setInvoiceId) => {
+const display = (component,setState,invoiceId,setInvoiceId,loading,setLoading,customerId,setCustomerId) => {
   switch(component) {
     case
      "Customers":
-      return <Customers state={component} setState={setState}/>
+      return <Customers state={component} setState={setState} loading={loading} setLoading={setLoading} setCustomerId={setCustomerId}/>
     case
      "Invoices":
-      return <Invoices state={component} setState={setState} setInvoiceId={setInvoiceId}/> 
+      return <Invoices state={component} setState={setState} setInvoiceId={setInvoiceId} loading={loading} setLoading={setLoading}/> 
     case
       "DetailedInvoice":
-       return <DetailedInvoice state={component} invoiceId={invoiceId}/>
+       return <DetailedInvoice state={component} invoiceId={invoiceId} setState={setState}/>
     case
       "DetailedCustomer":
-       return <DetailedCustomer state={component}/>  
+       return <DetailedCustomer state={component} customerId={customerId}/>  
     default: 
     return <Dashboard/>  
   }
