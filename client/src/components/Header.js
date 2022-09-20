d146c06efdfc073c92910e93f090596cc2757131
Saurabh Dabas <developerdabas@gmail.com>
2022-09-20 13:15:11 -0600

    Add react icon for logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GrNorton } from "react-icons/gr";
 const Header = () => {
 
   const clearLocalStorage = () => {
@@ -10,11 +11,11 @@ const Header = () => {
     <div className='header'>
       <header className='header__header'>
         <span className='header__brand'>
-          <img src='./bill.png' alt='brand-logo' width='50' height='50'/>
+          <GrNorton size={30}/>
           <h2>Easy Invoice</h2>
         </span>
         <Link className="header__button" to="/" onClick={clearLocalStorage}>
-          <img src={JSON.parse(localStorage.getItem('user')).avatar} alt='avatar' width='50' height='40'/>
+          <img src={JSON.parse(localStorage.getItem('user')).avatar} alt='avatar' width='30' height='30'/>
           <h4>Logout</h4>
 	      </Link>
       </header>
