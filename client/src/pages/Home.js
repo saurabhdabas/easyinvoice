eb6c499ea28d44d5bd9d50704cacb446be6d8c35
Saurabh Dabas <developerdabas@gmail.com>
2022-09-20 13:10:11 -0600

    Add new props

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,14 @@ import SideNavigation from '../components/SideNavigation'
 import display from '../Helpers/display';
 
 
-const Home = ({state,setState,history,className,setClassName,loading,setLoading,customerId,setCustomerId}) => {
+const Home = ({state,setState,history,className,setClassName,loading,setLoading,customerId,setCustomerId,invoiceId,setInvoiceId}) => {
 
   return (
     <div>
       <Header/>
       <div className='main'>
         <SideNavigation state={state} setState={setState} history={history} className={className} setClassName={setClassName}/>
-        {display(state[0],setState,loading,setLoading,customerId,setCustomerId)}
+        {display(state[0],setState,loading,setLoading,customerId,setCustomerId,invoiceId,setInvoiceId)}
       </div>
     </div>
   )
