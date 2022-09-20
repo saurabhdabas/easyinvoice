4dddbd183bc770dde12aecac0655e62f93760af2
Saurabh Dabas <developerdabas@gmail.com>
2022-09-20 13:08:01 -0600

    Add new seeds to the customers entity

--- server/db/seeds/02_customers.sql
+++ server/db/seeds/02_customers.sql
@@ -1,3 +1,3 @@
 -- seeds/02_customers.sql
 -- users customers
-INSERT INTO customers (id, name, photo, email, country, street, city, province, zipcode, company, logo, taxnumber) VALUES (1,'saurabh dabas', 'https://i.pravatar.cc/150?img=3', 'saurabhdabas@gmail.com','Canada', '56 ST NW', 'Calgary', 'Alberta','T3N 0P3', 'Computronix','https://www.computronix.com/wp-content/uploads/2017/08/cx-icon@2x.png', '063-8765');
+-- INSERT INTO customers (id, name, photo,phone,date,email, country, street, city, province, zipcode, company, logo, taxnumber) VALUES (1,'saurabh dabas','https://i.pravatar.cc/150?img=3','645-271-0840','2022-09-19','saurabhdabas@gmail.com','Canada', '56 ST NW', 'Calgary', 'Alberta','T3N 0P3', 'Computronix','https://www.computronix.com/wp-content/uploads/2017/08/cx-icon@2x.png', '063-8765');
