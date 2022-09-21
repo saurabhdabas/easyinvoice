4bfea718d16c6c3a0c8fda9ca9b89d4354dc5df0
Saurabh Dabas <developerdabas@gmail.com>
2022-09-21 13:02:25 -0600

    Add React icons

--- client/src/components/AddCustomer.js
+++ client/src/components/AddCustomer.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import AddCustomerForm from './AddCustomerForm';
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
-
-const AddCustomer = ({customers,setCustomers,showForm,setShowForm,inputs,setInputs,updateCustomerId,setUpdateCustomerId}) => {  
+const AddCustomer = ({customers,setCustomers,showForm,setShowForm,inputs,setInputs,updateCustomerId,setUpdateCustomerId,loading,setLoading,todaysDate}) => {  
   return (
     <React.Fragment>
       {!showForm ?
         <div className='customers__AddCustomer'>
-          <img src='./plus.png' alt='add-icon' width='50' height='50' onClick={()=>{
+          <AiOutlinePlusCircle size={50} onClick={()=>{
             setInputs({
               name:'',
+              photo:'',
+              phone:'',
+              date:todaysDate,
               email:'',
               country:'',
               street:'',
@@ -17,13 +20,14 @@ const AddCustomer = ({customers,setCustomers,showForm,setShowForm,inputs,setInpu
               province:'',
               zipcode:'',
               company:'',
+              logo:'',
               taxnumber:''
             })
             setShowForm(true)
             }}/>
           <h2>Add a new customer</h2>
         </div> : 
-        <AddCustomerForm customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm} inputs={inputs} setInputs={setInputs} updateCustomerId={updateCustomerId} setUpdateCustomerId={setUpdateCustomerId}/>
+        <AddCustomerForm customers={customers} setCustomers={setCustomers} showForm={showForm} setShowForm={setShowForm} inputs={inputs} setInputs={setInputs} updateCustomerId={updateCustomerId} setUpdateCustomerId={setUpdateCustomerId} loading={loading} setLoading={setLoading} todaysDate={todaysDate}/>
       }
     </React.Fragment>
   )
