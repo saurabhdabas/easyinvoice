43246487b6a559a66f9140b68b1166c719d061bb
Saurabh Dabas <developerdabas@gmail.com>
2022-09-21 13:02:48 -0600

    Add react icons and new input fields

--- client/src/components/AddInvoiceForm.js
+++ client/src/components/AddInvoiceForm.js
@@ -1,6 +1,22 @@
 import React, {useEffect } from 'react'
 import axios from 'axios';
 import findSubtotal from '../Helpers/findSubtotal';
+import { 
+  AiOutlineIdcard,
+  AiOutlineMail,
+  AiOutlinePicture,
+  AiOutlineShop,
+  AiOutlineNumber,
+  AiOutlineEnvironment,
+  AiOutlineFlag,
+  AiOutlineArrowLeft,
+  AiOutlinePhone,
+  AiOutlineCalendar,
+  AiOutlineTag,
+  AiOutlineMessage,
+  AiOutlineCheckCircle
+  } from "react-icons/ai";
+
 const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm,invoiceInputs,setInvoiceInputs,updateInvoiceId,setUpdateInvoiceId,loading,setLoading}) => {
 
   useEffect(()=>{
@@ -82,132 +98,134 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
       <div className="invoice-info">
       <div className='invoice-info__header'>
         <h1 className='invoice-info__form-title'>Invoice Information</h1>
-        <button className='invoice-info__back-btn' type='submit' onClick={handleNavigation}>Go back</button>
+        <button className='invoice-info__back-btn' type='submit' onClick={handleNavigation}>
+          <AiOutlineArrowLeft size={30}/>
+        </button>
       </div>
       <form className='invoice-info__form' onSubmit={handleSubmission}>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='name'><h3>Bill To</h3></label>
-            <img src='/id.png' alt='name-logo' width='20' height='20'/>
+            <AiOutlineIdcard color={'#2287E3'} size={20}/>
           </span>
           <input id='name' type='text' name='name' value={invoiceInputs.name} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,name:event.target.value})} placeholder='Jane thomas'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='photo'><h3>Photo</h3></label>
-            <img src='/id.png' alt='photo-logo' width='20' height='20'/>
+            <AiOutlinePicture color={'#2287E3'} size={20}/>
           </span>
           <input id='photo' type='text' name='photo' value={invoiceInputs.photo} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,photo:event.target.value})} placeholder='Paste an image URL'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='company'><h3>Company logo</h3></label>
-            <img src='/office.png' alt='company-logo' width='20' height='20'/>
+            <AiOutlinePicture color={'#2287E3'} size={20}/>
           </span>
           <input id='logo' type='text' name='logo' value={invoiceInputs.logo} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,logo:event.target.value})} placeholder='Paste an image URL'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='company'><h3>Company</h3></label>
-            <img src='/office.png' alt='company-logo' width='20' height='20'/>
+            <AiOutlineShop color={'#2287E3'} size={20}/>
           </span>
           <input id='name' type='text' name='company' value={invoiceInputs.company} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,company:event.target.value})} placeholder='Microsoft'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='taxnumber'><h3>Tax number</h3></label>
-            <img src='/tax.png' alt='taxnumber-logo' width='20' height='20'/>
+            <AiOutlineNumber color={'#2287E3'} size={20}/>
           </span>
           <input id='taxnumber' type='text' name='taxnumber' value={invoiceInputs.taxnumber} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,taxnumber:event.target.value})} placeholder='002-9865'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='email'><h3>Email id</h3></label>
-            <img src='/invoice-id.png' alt='taxnumber-logo' width='20' height='20'/>
+            <AiOutlineMail color={'#2287E3'} size={20}/>
           </span>
           <input id='email' type='text' name='email' value={invoiceInputs.email} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,email:event.target.value})} placeholder='janethomas@gmail.com'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='phone'><h3>Phone number</h3></label>
-            <img src='/tag.png' alt='invoice-logo' width='20' height='20'/>
+            <AiOutlinePhone color={'#2287E3'} size={20}/>
           </span>
           <input id='phone' type='tel' name='phone' value={invoiceInputs.phone} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,phone:event.target.value})} placeholder='438-650-0271'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='date'><h3>Invoice Date</h3></label>
-            <img src='/calendar.png' alt='date-logo' width='20' height='20'/>
+            <AiOutlineCalendar color={'#2287E3'} size={20}/>
           </span>
           <input id='date' type='date' name='date' value={invoiceInputs.date} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,date:event.target.value})}/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='duedate'><h3>Due Date</h3></label>
-            <img src='/calendar.png' alt='duedate-logo' width='20' height='20'/>
+            <AiOutlineCalendar color={'#2287E3'} size={20}/>
           </span>
           <input id='duedate' type='date' name='duedate' value={invoiceInputs.duedate} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,duedate:event.target.value})} />
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='title'><h3>Invoice title</h3></label>
-            <img src='/tag.png' alt='title-logo' width='20' height='20'/>
+            <AiOutlineTag color={'#2287E3'} size={20}/>
           </span>
           <input id='title' type='text' name='title' value={invoiceInputs.title} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,title:event.target.value})} placeholder='website redesigning'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='street'><h3>Street</h3></label>
-            <img src='/street.png' alt='street-logo' width='20' height='20'/>
+            <AiOutlineEnvironment color={'#2287E3'} size={20}/>
           </span>
           <input id='street' type='text' name='street' value={invoiceInputs.street} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,street:event.target.value})} placeholder='56 Ave NW'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='city'><h3>City</h3></label>
-            <img src='/city.png' alt='city-logo' width='20' height='20'/>
+            <AiOutlineEnvironment color={'#2287E3'} size={20}/>
           </span>
           <input id='city' type='text' name='city' value={invoiceInputs.city} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,city:event.target.value})} placeholder='Calgary'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='zipcode'><h3>Zipcode</h3></label>
-            <img src='/pincode.png' alt='zipcode-logo' width='20' height='20'/>
+            <AiOutlineEnvironment color={'#2287E3'} size={20}/>
           </span>
           <input id='zipcode' type='text' name='zipcode' value={invoiceInputs.zipcode} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,zipcode:event.target.value})} placeholder='T3K 0T4'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='province'><h3>Province</h3></label>
-            <img src='/state.png' alt='province-logo' width='20' height='20'/>
+            <AiOutlineEnvironment color={'#2287E3'} size={20}/>
           </span>
           <input id='province' type='text' name='province' value={invoiceInputs.province} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,province:event.target.value})} placeholder='Alberta'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='country'><h3>Country</h3></label>
-            <img src='/flag.png' alt='country-logo' width='20' height='20'/>
+            <AiOutlineFlag color={'#2287E3'} size={20}/>
           </span>
           <input id='country' type='text' name='country' value={invoiceInputs.country} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,country:event.target.value})} placeholder='Canada'/>
         </div>        
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='message'><h3>Message</h3></label>
-            <img src='/message.png' alt='country-logo' width='20' height='20'/>
+            <AiOutlineMessage color={'#2287E3'} size={20}/>
           </span>
           <input id='message' type='text' name='message' value={invoiceInputs.message} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,message:event.target.value})} placeholder='Enter your message'/>
         </div>
         <div className='invoice-info__input'>
           <span>
             <label htmlFor='remarks'><h3>Remarks</h3></label>
-            <img src='/message.png' alt='country-logo' width='20' height='20'/>
+            <AiOutlineMessage color={'#2287E3'} size={20}/>
           </span>
           <input id='remarks' type='text' name='remarks' value={invoiceInputs.notes} required onChange={(event)=>setInvoiceInputs({...invoiceInputs,notes:event.target.value})} placeholder='Additional Remarks'/>
         </div>
         <div className='invoice-info__input-table'>
           <div>
             <h3>Services</h3>
-            <img src='/creative.png' alt='services-logo' width='20' height='20'/>
+            <AiOutlineCheckCircle color={'#2287E3'} size={20}/>
           </div>
           <table className='invoice-table'>
             <thead>
@@ -323,7 +341,7 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
                 <td></td>
                 <td></td>
                 <td>Subtotal</td>
-                <td>{invoiceInputs.subtotal}</td>
+                <td>$&nbsp;{invoiceInputs.subtotal}</td>
               </tr>
               <tr className='invoice-table__footer'>
                 <td></td>
@@ -341,7 +359,7 @@ const AddInvoiceForm = ({invoices,setInvoices,showInvoiceForm,setShowInvoiceForm
                 <td></td>
                 <td></td>
                 <td>Balance</td>
-                <td>{invoiceInputs.balance}</td>
+                <td>$&nbsp;{invoiceInputs.balance}</td>
               </tr>
             </tfoot>
           </table>
