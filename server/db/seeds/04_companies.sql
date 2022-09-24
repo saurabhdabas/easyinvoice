43acb4e66a05ccf54d479f923cf100908b9d023a
Saurabh Dabas <developerdabas@gmail.com>
2022-09-23 21:25:39 -0600

    add seeds for companies

---
+++ server/db/seeds/04_companies.sql
@@ -0,0 +1,5 @@
+-- seeds/02_customers.sql
+-- users customers
+INSERT INTO companies (company_name,company_logo,taxnumber,street,city,zipcode,province,country) VALUES ('Computronix','https://www.computronix.com/wp-content/uploads/2017/08/cx-icon@2x.png','638-589','80Ave NW','Calgary','T3N 0P3','Alberta','Canada');
+
+INSERT INTO companies (company_name,company_logo,taxnumber,street,city,zipcode,province,country) VALUES ('Computronix','https://www.computronix.com/wp-content/uploads/2017/08/cx-icon@2x.png','638-589','80Ave NW','Calgary','T3N 0P3','Alberta','Canada');
