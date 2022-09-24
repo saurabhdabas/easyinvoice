694da3162b903bf25e26257bf608077f152a88e7
Saurabh Dabas <developerdabas@gmail.com>
2022-09-24 11:23:13 -0600

    Add orders entity

---
+++ server/db/schema/05_create_orders.sql
@@ -0,0 +1,11 @@
+-- schema/02_create_orders.sql
+DROP TABLE IF EXISTS orders CASCADE;
+-- CREATE ORDERS
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  order_date VARCHAR(255) NOT NULL,
+  order_month VARCHAR(255) NOT NULL,
+  order_status VARCHAR(255) NOT NULL,
+  order_amount INTEGER NOT NULL,
+  customer_id INTEGER  NOT NULL REFERENCES customers(id) ON DELETE CASCADE
+);
