05b40b438d2eeaaafc842d3ffee4e374849baed6
Saurabh Dabas <developerdabas@gmail.com>
2022-09-24 11:22:53 -0600

    seeds for order entity

---
+++ server/db/seeds/05_orders.sql
@@ -0,0 +1,15 @@
+-- seeds/05_orders.sql
+-- users orders
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (1,'Sep 18, 2022','Jan','Paid',4350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (2,'Sep 18, 2022','Jan','Unpaid',1350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (3,'Sep 18, 2022','Jan','Unpaid',3350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (4,'Sep 18, 2022','Jan','Unpaid',2350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (5,'Sep 18, 2022','Jan','Unpaid',4350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (6,'Sep 18, 2022','Jan','Unpaid',1350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (7,'Sep 18, 2022','Jan','Paid',4350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (8,'Sep 18, 2022','Jan','Unpaid',1350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (9,'Sep 18, 2022','Jan','Unpaid',3350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (10,'Sep 18, 2022','Jan','Unpaid',2350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (11,'Sep 18, 2022','Jan','Paid',4350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (12,'Sep 18, 2022','Jan','Unpaid',1350,1);
+INSERT INTO orders (order_id, order_date, order_month,order_status,order_amount,customer_id) VALUES (13,'Sep 18, 2022','Jan','Unpaid',350,2);
