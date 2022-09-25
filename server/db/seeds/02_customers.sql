cb812371953a424188be66d15d24a2490d08d03d
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 00:13:19 -0600

    Add seeds for firstname and lastname

--- server/db/seeds/02_customers.sql
+++ server/db/seeds/02_customers.sql
@@ -1,5 +1,5 @@
 -- seeds/02_customers.sql
 -- users customers
-INSERT INTO customers (photo,fullname,phonenumber,joiningdate,email) VALUES ('https://i.pravatar.cc/150?img=6','saurabh dabas','645-271-0840','Sep 18, 2022','saurabhdabas@gmail.com');
+-- INSERT INTO customers (photo,firstname,lastname,phonenumber,joiningdate,email) VALUES ('https://i.pravatar.cc/150?img=6','saurabh','dabas','645-271-0840','Sep 18, 2022','saurabhdabas@gmail.com');
 
-INSERT INTO customers (photo,fullname,phonenumber,joiningdate,email) VALUES ('https://i.pravatar.cc/150?img=5','Eleanor Martine','645-271-0840','Sep 18, 2022','saurabhdabas@gmail.com');
+-- INSERT INTO customers (photo,firstname,lastname,phonenumber,joiningdate,email) VALUES ('https://i.pravatar.cc/150?img=5','Eleanor' ,'Martine','645-271-0840','Sep 18, 2022','saurabhdabas@gmail.com');
