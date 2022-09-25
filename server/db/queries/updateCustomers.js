97ea9038907fda73cdd82d02de7578212639485a
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 00:18:28 -0600

    Add firstname and lastname to query

--- server/db/queries/updateCustomers.js
+++ server/db/queries/updateCustomers.js
@@ -2,8 +2,8 @@ const db = require('../../configs/db.config');
 
 const updateCustomers = (formData) => {
 	return db.query(
-  `UPDATE customers SET photo=$2,fullname=$3,phonenumber=$4,email=$5
-  WHERE id = ($1) RETURNING *;`, [formData.customerId,formData.photo,formData.fullname,formData.phonenumber,formData.email])
+  `UPDATE customers SET photo=$2,firstname=$3, lastname=$4, phonenumber=$5,email=$6
+  WHERE id = ($1) RETURNING *;`, [formData.customerId,formData.photo,formData.firstname,formData.lastname,formData.phonenumber,formData.email])
   .then(data => {
 		return data.rows;
 	})
