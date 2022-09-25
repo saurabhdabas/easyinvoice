b3c716bdae499c3d90f8e414981c6293e37569b1
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 00:16:23 -0600

    Add firstname and lastname to query

--- server/db/queries/addToCustomers.js
+++ server/db/queries/addToCustomers.js
@@ -2,9 +2,9 @@ const db = require('../../configs/db.config');
 
 const addToCustomers = (formData) => {
 	return db.query(
-    `INSERT INTO customers(photo,fullname,phonenumber,email,joiningdate)
-    VALUES($1, $2, $3, $4, $5) RETURNING *`,
-    [formData.photo,formData.fullname,formData.phonenumber,formData.email,formData.joiningdate]).then(data => {
+    `INSERT INTO customers(photo,firstname,lastname,phonenumber,email,joiningdate)
+    VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
+    [formData.photo,formData.firstname,formData.lastname,formData.phonenumber,formData.email,formData.joiningdate]).then(data => {
 		return data.rows;
 	})
 }
