d305301b5f99209e5501db0c8522929783b8dbaf
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 00:13:43 -0600

    Add firstname and lastname

--- server/db/schema/02_create_customers.sql
+++ server/db/schema/02_create_customers.sql
@@ -4,7 +4,8 @@ DROP TABLE IF EXISTS customers CASCADE;
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   photo VARCHAR(255) NOT NULL,
-  fullname VARCHAR(255) NOT NULL,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
   phonenumber VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   joiningdate VARCHAR(255) NOT NULL
