0258e99def1ae463e2351b0bc6fe4a91752d214a
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 11:53:30 -0600

    Fix query

--- server/db/queries/getAllOrders.js
+++ server/db/queries/getAllOrders.js
@@ -1,7 +1,7 @@
 const db = require('../../configs/db.config');
 
 const getAllOrders = () => {
-	return db.query("SELECT * FROM orders;").then(data => {
+	return db.query("SELECT * FROM orders JOIN payments ON order_id = payments.orderId JOIN customers ON customerId = customers.id;").then(data => {
 		return data.rows;
 	})
 }
