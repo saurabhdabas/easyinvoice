053ccb412f7a502fdc8bd384d6755e5f268728e4
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 01:01:39 -0600

    Add Orders

--- client/src/components/SideNavigation.js
+++ client/src/components/SideNavigation.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {useNavigate} from 'react-router-dom';
 import { BsPeopleFill } from "react-icons/bs";
-import { AiFillLayout, AiFillSnippets, AiFillCaretDown } from "react-icons/ai";
+import { GrNorton } from "react-icons/gr";
+import { AiFillLayout, AiFillSnippets, AiFillCaretDown, AiFillDatabase } from "react-icons/ai";
 
 const SideNavigation = ({state,setState,history}) => {
   const navigate = useNavigate();
@@ -11,44 +13,62 @@ const SideNavigation = ({state,setState,history}) => {
   }
   return (
     <div className='sideNavigation'>
-      <ul className='sideNavigation__items'>
-        <li 
-        aria-label='Dashboard' 
-        className={state.includes('Dashboard') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
-        onClick={
-          (event)=>{
-            setState([event.currentTarget.ariaLabel])
-            history.push("/dashboard", { value: event.currentTarget.ariaLabel });
-          }}
-        >
-          <AiFillLayout size={30}/>
-          <h3>Dashboard</h3>
-        </li>
-        <li 
-        aria-label='Customers' 
-        className={state.includes('Customers')  || state.includes('DetailedCustomer')? 'sideNavigation__item active' : 'sideNavigation__item'} 
-        onClick={
-          (event)=>{
-            setState([event.currentTarget.ariaLabel])
-            history.push("/customers", { value: event.currentTarget.ariaLabel });
-          }}
-        >
-          <BsPeopleFill size={30}/>
-          <h3>Customers</h3>
-        </li>
-        <li 
-        aria-label='Invoices' 
-        className={state.includes('Invoices') || state.includes('DetailedInvoice')? 'sideNavigation__item active' : 'sideNavigation__item'} 
-        onClick={
-          (event)=>{
-            setState([event.currentTarget.ariaLabel])
-            history.push("/invoices", { value: event.currentTarget.ariaLabel });
-          }}
-        >
-          <AiFillSnippets size={30}/>
-          <h3>Invoices</h3>
-        </li>
-      </ul>
+      <div className='sideNavigation__header'>
+        <div className='sideNavigation__brand'>
+          <GrNorton size={40}/>
+          <h2>Easy Invoice</h2>
+        </div>
+        <ul className='sideNavigation__items'>
+          <li 
+          aria-label='Dashboard' 
+          className={state.includes('Dashboard') ? 'sideNavigation__item active' : 'sideNavigation__item'} 
+          onClick={
+            (event)=>{
+              setState([event.currentTarget.ariaLabel])
+              history.push("/dashboard", { value: event.currentTarget.ariaLabel });
+            }}
+          >
+            <AiFillLayout size={30} style={{marginRight:'10'}}/>
+            <h3>Dashboard</h3>
+          </li>
+          <li 
+          aria-label='Customers' 
+          className={state.includes('Customers')  || state.includes('DetailedCustomer')? 'sideNavigation__item active' : 'sideNavigation__item'} 
+          onClick={
+            (event)=>{
+              setState([event.currentTarget.ariaLabel])
+              history.push("/customers", { value: event.currentTarget.ariaLabel });
+            }}
+          >
+            <BsPeopleFill size={30}/>
+            <h3>Customers</h3>
+          </li>
+          <li 
+          aria-label='Invoices' 
+          className={state.includes('Invoices') || state.includes('DetailedInvoice')? 'sideNavigation__item active' : 'sideNavigation__item'} 
+          onClick={
+            (event)=>{
+              setState([event.currentTarget.ariaLabel])
+              history.push("/invoices", { value: event.currentTarget.ariaLabel });
+            }}
+          >
+            <AiFillSnippets size={30}/>
+            <h3>Invoices</h3>
+          </li>
+          <li 
+          aria-label='Orders' 
+          className={state.includes('Orders') || state.includes('DetailedOrder')? 'sideNavigation__item active' : 'sideNavigation__item'} 
+          onClick={
+            (event)=>{
+              setState([event.currentTarget.ariaLabel])
+              history.push("/orders", { value: event.currentTarget.ariaLabel });
+            }}
+          >
+            <AiFillDatabase size={30}/>
+            <h3>Orders</h3>
+          </li>
+        </ul>
+      </div>
       <div className="sideNavigation__account">
         <div className="sideNavigation__userImage" to="/" onClick={clearLocalStorage}>
           <img src={JSON.parse(localStorage.getItem('user')).avatar} alt='avatar' width='50' height='50'/>
@@ -56,6 +76,9 @@ const SideNavigation = ({state,setState,history}) => {
         <div className="sideNavigation__account-description">
           <h3>Account&nbsp;<AiFillCaretDown/></h3>
           <h4>{JSON.parse(localStorage.getItem('user')).email}</h4>
+          <Link className="sideNavigation__logout" to="/" onClick={clearLocalStorage}>
+          <h4>Logout</h4>
+	      </Link>
         </div>
       </div>
     </div>
