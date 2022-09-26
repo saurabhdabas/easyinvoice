24315f04dba0444c417ecbd1b43bb72fd7295306
Saurabh Dabas <developerdabas@gmail.com>
2022-09-25 22:38:43 -0600

    Fix query to fetch apt data

--- server/db/queries/getUnpaidInvoices.js
+++ server/db/queries/getUnpaidInvoices.js
@@ -2,7 +2,7 @@ const db = require('../../configs/db.config');
 
 const getUnpaidInvoices = () => {
 	return db.query(
-    "SELECT * FROM invoices JOIN orders ON orderId = orders.order_id JOIN customers ON customers.id = orders.customer_id WHERE order_status = 'Unpaid';")
+    "SELECT * FROM invoices JOIN orders ON orderId = orders.order_id JOIN customers ON customers.id = orders.customer_id JOIN payments on payment_id = invoice_id WHERE payment_status = 'UnPaid';")
     .then(data => {
 		return data.rows;
 	})
