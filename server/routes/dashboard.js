d38944d5447099f878661e74fcb6827daabc136e
Saurabh Dabas <developerdabas@gmail.com>
2022-09-28 13:57:58 -0600

    Fix query at the route

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const paidInvoices = require('../db/queries/getPaidInvoices');
 const unpaidInvoices = require('../db/queries/getUnpaidInvoices');
+const partialInvoices = require('../db/queries/getPartialInvoices');
 const invoicesWithOrders = require('../db/queries/getInvoicesWithOrderStatus');
 module.exports = (db) => {
   // all routes will go here 
@@ -9,8 +10,9 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     const invoices = unpaidInvoices.getUnpaidInvoices();
     const paidOffInvoices = paidInvoices.getPaidInvoices();
+    const partials = partialInvoices.getPartialInvoices();
     const invoicesWithOrdersStatus = invoicesWithOrders.getInvoicesWithOrderStatus(); 
-    Promise.all([invoices,invoicesWithOrdersStatus,paidOffInvoices])
+    Promise.all([invoices,invoicesWithOrdersStatus,paidOffInvoices,partials])
     .then(data => {
       console.log(data)
       res.json(data);
