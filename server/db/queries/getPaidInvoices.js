910f2394f65fd384cd52e01ae22983ca26144500
Saurabh Dabas <developerdabas@gmail.com>
2022-09-28 13:56:09 -0600

    Fix queries

--- server/db/queries/getPaidInvoices.js
+++ server/db/queries/getPaidInvoices.js
@@ -2,7 +2,7 @@ const db = require('../../configs/db.config');
 
 const getPaidInvoices = () => {
 	return db.query(
-    "SELECT * FROM invoices JOIN orders ON orderId = orders.order_id JOIN customers ON customers.id = orders.customer_id JOIN payments on payment_id = invoice_id WHERE payment_status = 'Paid';")
+    "SELECT * FROM invoices JOIN customers ON customers.id = invoices.customerId JOIN payments on payment_id = invoice_id WHERE payment_status = 'Paid';")
     .then(data => {
 		return data.rows;
 	})
--- server/db/queries/getUnpaidInvoices.js
+++ server/db/queries/getUnpaidInvoices.js
@@ -2,7 +2,7 @@ const db = require('../../configs/db.config');
 
 const getUnpaidInvoices = () => {
 	return db.query(
-    "SELECT * FROM invoices JOIN orders ON orderId = orders.order_id JOIN customers ON customers.id = orders.customer_id JOIN payments on payment_id = invoice_id WHERE payment_status = 'UnPaid';")
+    "SELECT * FROM invoices JOIN customers ON customers.id = invoices.customerId JOIN payments on payment_id = invoice_id WHERE payment_status = 'UnPaid';")
     .then(data => {
 		return data.rows;
 	})
